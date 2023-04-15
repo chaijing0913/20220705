75721a3b627ebb687b712e9cb52b2e0579d35fa5
chaijing0913 <3214717812@qq.com>
2023-04-16 07:34:26 +0800

    提交另个

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int z=(byte) 128;
+        System.out.println(z);
+        double a=2.55;
+        float b=0;
+        //b=a;
+        System.out.println(b+" "+a);
+    }
+}
